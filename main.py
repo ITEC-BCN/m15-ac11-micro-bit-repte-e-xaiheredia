e57d2614d80c8b86bd5caf3a045fbcda17009d3b
xavihe1 <146827255+xavihe1@users.noreply.github.com>
2024-11-03 23:03:24 +0100

    Actualizar pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+from microbit import *
 
+x = 2
+y = 2
+MAX_TEMP = 50
+
+
+def grafica_temperatura():
+    temperatura = temperature()  
+    display.clear()  
+    display.plot_bar_graph(temperatura, MAX_TEMP)  
+
+def moure_gota():
+    global x, y  
+    
+    acc_x = accelerometer.get_x()
+    acc_y = accelerometer.get_y()
+    
+    if acc_x <= -150 and x > 0:
+        x -= 1
+    elif acc_x >= 150 and x < 4:
+        x += 1
+    
+    if acc_y <= -150 and y > 0:
+        y -= 1
+    elif acc_y >= 150 and y < 4:
+        y += 1
+    
+    display.set_pixel(x, y, 9)
+    sleep(50)
+    display.set_pixel(x, y, 0)
+
+while True:
+    grafica_temperatura()
+    moure_gota()
+    sleep(200)  
--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
+let x = 2
+let y = 2
+function grafica_temperatura() {
+    let temperatura = input.temperature()
+}
 
